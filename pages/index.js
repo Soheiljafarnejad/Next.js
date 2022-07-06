b502420d766b200f099c038330431a3fad8f8d73
soheiljafarnejad <jafarnejad.official@gmail.com>
2022-07-06 20:16:46 +0430

    getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,23 @@
-export default function Home() {
-  return <div>home page</div>;
+import axios from "axios";
+
+export default function Home({ userList }) {
+  return (
+    <div>
+      <h2>home page</h2>
+      <ul>
+        {userList.map((item) => {
+          return <li key={item.id}>{item.title}</li>;
+        })}
+      </ul>
+    </div>
+  );
+}
+
+export async function getStaticProps() {
+  const { data } = await axios.get("https://jsonplaceholder.typicode.com/posts");
+  return {
+    props: {
+      userList: data.slice(1, 10),
+    },
+  };
 }
