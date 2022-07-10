65a093f640668f402f15d482f2fc8ac8a84ef6ad
soheiljafarnejad <jafarnejad.official@gmail.com>
2022-07-10 18:24:22 +0430

    fallback true

--- pages/episode/[episodeId].js
+++ pages/episode/[episodeId].js
@@ -1,6 +1,11 @@
 import axios from "axios";
+import { useRouter } from "next/router";
 
 const Episode = ({ episode }) => {
+  const router = useRouter();
+
+  if (router.isFallback) return <h2>loading...</h2>;
+
   return (
     <div>
       {episode.id} / name :{episode.name} - date: {episode.air_date}
@@ -12,13 +17,19 @@ export default Episode;
 
 export async function getStaticPaths() {
   const { data } = await axios.get(`https://rickandmortyapi.com/api/episode`);
-  const paths = data.results.map((item) => {
-    return { params: { episodeId: item.id.toString() } };
-  });
+
+  // const paths = data.results.map((item) => {
+  //   return { params: { episodeId: item.id.toString() } };
+  // });
 
   return {
-    paths,
-    fallback: false,
+    paths: [
+      { params: { episodeId: "1" } },
+      { params: { episodeId: "2" } },
+      { params: { episodeId: "3" } },
+      { params: { episodeId: "4" } },
+    ],
+    fallback: true,
   };
 }
 
